a0c707592a3bc7bb38851cf3d06786e0dca3e5c8
vampyrsoda <72835263+vampyrsoda@users.noreply.github.com>
2021-06-11 22:07:15 -0300

    conclui / deleta tarefas

--- #notebook/componentes/concluiTarefas.js
+++ #notebook/componentes/concluiTarefas.js
@@ -9,3 +9,14 @@ const BotaoConclui = () => {
 
     return botaoConclui;
 };
+
+const concluirTarefa = (evento) => {
+
+    const botaoConclui = evento.target;
+
+    const tarefaCompleta = botaoConclui.parentElement;
+
+    tarefaCompleta.classList.toggle('done');
+};
+
+export default BotaoConclui;
